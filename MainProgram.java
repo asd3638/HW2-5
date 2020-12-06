224794164e084054133141f4a07c0e662a2f7a87
nahui <hui999hui@naver.com>
2020-12-06 16:59:18 +0900

    1810666

--- MainProgram.java
+++ MainProgram.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MainProgram {
@@ -25,7 +26,77 @@ public class MainProgram {
     public void runModule(int studentID) {
         if (studentID == 11530)
             System.out.println("No. It is the professor ID.\n");
+        else if(studentID==1810666)
+           hw_1810666();
         else
             System.out.println("To be developed...\n");
     }
+    
+    public void hw_1810666() {
+       int choice;
+       int n1;int n2; int n3;
+       int fact;
+       try {
+          Scanner n_scan=new Scanner(System.in);
+          System.out.println("[Student ID: 1810666]");
+          System.out.println("1. Calculate Max");
+          System.out.println("2. Calculate Factorial");
+          System.out.println("-Input Only Integer Type");
+          System.out.println();
+          System.out.print("Enter menu number: ");
+          choice= n_scan.nextInt();
+          System.out.println();
+          
+          if(choice==1) {
+             System.out.println("-Calculate Max-");
+             System.out.println("Input 3 Numbers.");
+             
+             System.out.print("First number:");
+             n1=n_scan.nextInt();
+             System.out.print("Second number:");
+             n2=n_scan.nextInt();
+             System.out.print("Third number:");
+             n3=n_scan.nextInt();
+             
+             System.out.println("=> Max Result: "+max_1810666(n1,n2,n3));
+          }
+          else if (choice==2) {
+             System.out.println("-Calculate Factorial-");
+             System.out.print("Input Positive Number:");
+             fact=n_scan.nextInt();
+             if(fact>0) {
+                //calculate factorial
+                 System.out.println("=> Factorial Result: "+fact_1810666(fact));
+             }
+             else {System.out.println("Wrong Input");}
+             
+          }
+          else {System.out.println("Wrong menu Input");}
+            System.out.println();
+       }
+       catch(InputMismatchException e) {
+          System.out.println("Wrong Input Type");
+          System.out.println();
+       }
+       
+       
+    }
+    
+    public int max_1810666(int n1,int n2,int n3){
+       int max;
+       max=((n1>n2)?n1:n2);
+       max=((n3>max)?n3:max);
+       return max;
+    }
+    
+    public int fact_1810666(int num) {
+       int i=1;
+       int fact_res=1;
+       for(i=num;i>0;i--) {
+          fact_res=fact_res*i;
+       }
+       return fact_res;
+    }
+    
+ 
 }
